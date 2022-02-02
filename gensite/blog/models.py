71c1d8c2e525584a09a401715feb5fd3cfcc460e
hossain <hossain258@gmail.com>
2022-02-02 16:17:26 +0000

    ckeditor added

--- gensite/blog/models.py
+++ gensite/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
 class aboutus(models.Model):
     title = models.CharField(max_length=200)
     heading = models.CharField(max_length=300)
-    description =models.TextField(max_length=700)
+    # description = models.TextField(max_length=1200)
+    description = RichTextField( blank =True, null = True)
     
     def __str__(self) :
         return self.title
@@ -13,13 +16,15 @@ class contactus(models.Model):
     name = models.CharField(max_length=25)
     email = models.CharField(max_length=20)
     phone_Number =  models.CharField(max_length=20, null =True)
-    message = models.TextField()
+    message = models.TextField(max_length=1200)
     date_created= models.DateTimeField(auto_now_add=True)
     
     
 class services(models.Model):
     title =models.CharField(max_length=100)
-    description =models.TextField(max_length=1000)
+    # description = models.TextField(max_length=1200)
+    description = RichTextField( blank =True, null = True)
+    
     
     def __str__(self) :
         return self.title
--- gensite/gensite/settings.py
+++ gensite/gensite/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'ckeditor',
+  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
