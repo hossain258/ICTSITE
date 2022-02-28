9cd3ba1c06ddfc5f4cb77418b5688f2968fe8955
monirtikweb <monir@tikweb.dk>
2022-02-28 23:56:06 +0600

    Design update

--- gensite/blog/admin.py
+++ gensite/blog/admin.py
@@ -3,13 +3,15 @@ from.models import aboutus
 from.models import contactus
 from.models import services
 from.models import clientdata
+from.models import  Dynamicslider
 
 # Register your models here.
 admin.site.register(aboutus)
 admin.site.register(services)
 admin.site.register(clientdata)
+admin.site.register(Dynamicslider)
 
-#admin.site.register(contactus)
+# admin.site.register(contactus)
 
 
 @admin.register(contactus)
